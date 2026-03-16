182d116318934fd2848a143582148df4490bcd8f
heyzone <harrywujy520@gmail.com>
2026-03-17 07:43:20 +0800

    Delete playwright.config.js

--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@playwright/test');
-module.exports = defineConfig({
-  testDir: './tests',
-  testMatch: '**/*.spec.js',
-  timeout: 120000,
-  use: {
-    headless: false,
-  },
-});
